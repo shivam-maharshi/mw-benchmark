61dc0d218fb885cf7c9e3eb931e71440aeead55d
shivam-maharshi <shivam.maharshi@gmail.com>
2016-03-02 21:57:50 -0500

    Added Java program to go through the complete trace and categorize URL's according to the status received.

--- src/main/java/com/yahoo/ycsb/RestClient.java
+++ src/main/java/com/yahoo/ycsb/RestClient.java
@@ -5,7 +5,9 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 import java.util.Vector;
@@ -22,6 +24,8 @@ import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.vt.edu.utils.Constant;
+import org.vt.edu.utils.FileUtil;
 
 /**
  * Class responsible for making web service requests for benchmarking purpose.
@@ -251,18 +255,27 @@ public class RestClient extends DB {
 		return rc;
 	}
 
-	public static void main(String[] args) throws TimeoutException, IOException {
-		String data = "Miusov, as a man man of breeding and deilcacy, could not but feel some inwrd qualms, when he reached the Father Superior's with Ivan: he felt ashamed of havin lost his temper. He felt that he ought to have disdaimed that despicable wretch, Fyodor Pavlovitch, too much to have been upset by him in Father Zossima's cell, and so to have forgotten himself. Teh monks were not to blame, in any case, he reflceted, on the steps.And if they're decent people here (and the Father Superior, I understand, is a nobleman) why not be friendly and courteous withthem? I won't argue, I'll fall in with everything, I'll win them by politness, and show them that I've nothing to do with that Aesop, thta buffoon, that Pierrot, and have merely been takken in over this affair, just as they have.";
-		String postData = new StringBuilder("section=new&title=")
-				.append("%CE%95%CE%BE%CE%AD%CE%B3%CE%B5%CF%81%CF%83%CE%B7_%CF%84%CE%BF%CF%85_%CE%A0%CE%BF%CE%BB%CF%85%CF%84%CE%B5%CF%87%CE%BD%CE%B5%CE%AF%CE%BF%CF%85")
-				.append("&appendtext=").append(data).append("&token=%2B%5C").toString();
+	public static void main(String[] args) {
+//		String data = "Miusov, as a man man of breeding and deilcacy, could not but feel some inwrd qualms, when he reached the Father Superior's with Ivan: he felt ashamed of havin lost his temper. He felt that he ought to have disdaimed that despicable wretch, Fyodor Pavlovitch, too much to have been upset by him in Father Zossima's cell, and so to have forgotten himself. Teh monks were not to blame, in any case, he reflceted, on the steps.And if they're decent people here (and the Father Superior, I understand, is a nobleman) why not be friendly and courteous withthem? I won't argue, I'll fall in with everything, I'll win them by politness, and show them that I've nothing to do with that Aesop, thta buffoon, that Pierrot, and have merely been takken in over this affair, just as they have.";
+//		String postData = new StringBuilder("section=new&title=")
+//				.append("%CE%95%CE%BE%CE%AD%CE%B3%CE%B5%CF%81%CF%83%CE%B7_%CF%84%CE%BF%CF%85_%CE%A0%CE%BF%CE%BB%CF%85%CF%84%CE%B5%CF%87%CE%BD%CE%B5%CE%AF%CE%BF%CF%85")
+//				.append("&appendtext=").append(data).append("&token=%2B%5C").toString();
 		RestClient rc = getClient();
 		HashMap<String, ByteIterator> result = new HashMap<String, ByteIterator>();
-		rc.httpGet(
-				"http://192.168.1.51:8080/wiki/index.php/"
-						+ "%CE%95%CE%BE%CE%AD%CE%B3%CE%B5%CF%81%CF%83%CE%B7_%CF%84%CE%BF%CF%85_%CE%A0%CE%BF%CE%BB%CF%85%CF%84%CE%B5%CF%87%CE%BD%CE%B5%CE%AF%CE%BF%CF%85",
-				result);
-		rc.httpPost("http://192.168.1.51:8080/wiki/api.php?action=edit&format=json", postData);
+		List<String> titles = FileUtil.read(Constant.RELATIVE_PATH+"readtrace.txt",10000);
+		int responseCode = 0;
+		for(String title: titles) {
+			try {
+				responseCode = rc.httpGet("http://192.168.1.51/wiki/index.php/",result);
+			} catch(IOException | TimeoutException e) {
+				e.printStackTrace();
+				responseCode = 500;
+			}
+			System.out.println("GET Request: " + "http://192.168.1.51:80/wiki/index.php/"+title + " | Response Code: " + responseCode);
+			if(responseCode==500) {
+			}
+		}
+		// rc.httpPost("http://192.168.1.51:8080/wiki/api.php?action=edit&format=json", postData);
 	}
 
 	class Timer implements Runnable {
--- src/main/java/org/vt/edu/utils/Constant.java
+++ src/main/java/org/vt/edu/utils/Constant.java
@@ -5,5 +5,6 @@ public class Constant {
 	public static final String GZIP_TYPE = ".gz";
 	public static final String TEXT_TYPE = ".txt";
 	public static final String RELATIVE_PATH = "C:/Users/Sam/Google Drive/Job/VirginiaTech/MS Thesis/YCSB4WebServices/input/";
+	public static final String OUTPUT_PATH = "C:/Users/Sam/Google Drive/Job/VirginiaTech/MS Thesis/YCSB4WebServices/output/";
 
 }
