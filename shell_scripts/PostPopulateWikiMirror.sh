c1ea9455d2ab8f1b9036317d26b0e3d4e5ea231e
shivam-maharshi <shivam.maharshi@gmail.com>
2016-08-19 16:19:06 -0400

    Added more cleanup scripts to be invoked post MW DB installation.

--- shell_scripts/PostPopulateWikiMirror.sh
+++ shell_scripts/PostPopulateWikiMirror.sh
@@ -1,7 +1,16 @@
-cd /var/www/html/mediawiki
+cd /var/www/html/mediawiki/maintenance
 
-sudo php maintenance/populateParentId.php
-sudo php maintenance/populateRevisionLength.php
-sudo php maintenance/populateRevisionSha1.php
-sudo php maintenance/update.php
-sudo php maintenance/rebuildall.php
+sudo php cleanupTitles.php
+sudo php deleteSelfExternals.php
+sudo php populateBacklinkNamespace.php
+sudo php populateParentId.php
+sudo php populateRevisionLength.php
+sudo php populateRevisionSha1.php
+sudo php tidyUpBug37714.php
+sudo php updateRestrictions.php
+sudo php updateSpecialPages.php
+sudo php updateDoubleWidthSearch.php
+# sudo php refreshLinks.php
+sudo php rebuildtextindex.php
+sudo php update.php
+sudo php rebuildall.php
