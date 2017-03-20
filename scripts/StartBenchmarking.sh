0574cf3e936b42e7a4b727a5f691575274993d8d
Shivam Maharshi <shivam.maharshi@gmail.com>
2017-03-20 00:17:58 -0400

    Delete StartBenchmarking.sh

--- scripts/StartBenchmarking.sh
+++
@@ -1,3 +0,0 @@
-cd ~/benchmarking/YCSB4WebServices/jar
-
-sudo java -Xms8096m -Xmx12086m -cp "YCSB4WebServices-0.0.jar" com.yahoo.ycsb.Client -s -t -P C:/Users/Sam/Google Drive/Job/VirginiaTech/MS Thesis/YCSB4WebServices/workload/workload -p threadcount=1 -p target=1 -p operationcount=10 -p exportfile=C:/Users/Sam/Google Drive/Job/VirginiaTech/MS Thesis/YCSB4WebServices/BenchmarkingResults.txt  
