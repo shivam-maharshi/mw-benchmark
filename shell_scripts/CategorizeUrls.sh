d2ac2c79139c03ce594578e22fcdb76bce7ab60b
Shivam Maharshi <shivam.maharshi@gmail.com>
2017-03-20 00:42:37 -0400

    Delete CategorizeUrls.sh

--- shell_scripts/CategorizeUrls.sh
+++
@@ -1,1 +0,0 @@
-sudo java -Xms3096m -Xmx1024m -cp "mw-benchmark-0.0.jar" org.vt.edu.utils.CategorizeByHttpStatus -ad=192.168.1.51:80/wiki -input=~/development/benchmarking/readtrace.txt -output=~/development/benchmarking/ -count=1000000 -offset=0 -workers=8
