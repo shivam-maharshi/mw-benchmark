bf505cb1888cc010d07be22ab11f10be1b76d005
Shivam Maharshi <shivam.maharshi@gmail.com>
2017-03-20 00:32:35 -0400

    Update PopulateWikiMirror.sh

--- scripts/cent_os_6.6/PopulateWikiMirror.sh
+++ scripts/cent_os_6.6/PopulateWikiMirror.sh
@@ -9,8 +9,6 @@ sudo wget "http://archive.org/download/elwiki-20151201/elwiki-20151201-pages-met
 sudo wget "http://archive.org/download/elwiki-20151201/elwiki-20151201-all-titles-in-ns0.gz"
 sudo bzip2 -dk elwiki-20151201-pages-meta-current.xml.bz2
 cd /var/www/html/mediawiki/maintenance
-# For using importDump for dumping.
 # sudo php importDump.php < /home/ubuntu/elwiki-20151201-pages-meta-history.xml >& progress.log &
-# For using MWImport for dumping.
 java -Xmx16396m -Xms1024m -XX:+UseParallelGC -server -jar mwdumper.jar --format=sql:1.5 /home/shivam/elwiki-20151201-pages-meta-current.xml.bz2 | mysql -u root -p wikimirror --default-character-set=utf8
 sudo tail -f progress.log
