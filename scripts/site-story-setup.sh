00330fbf1e677f00b450ce906179caba7534c5e9
Shivam Maharshi <shivam.maharshi@gmail.com>
2017-03-20 00:32:09 -0400

    Update and rename SiteStoryServerCommands.txt to site-story-setup.sh

--- scripts/site-story-setup.sh
+++ scripts/site-story-setup.sh
@@ -5,10 +5,6 @@ brew install httpd22
 brew install gcc48
 brew install git
 sudo sed -i '' "s/backup/$ARCHIVE_DIR/g" ~/development/apache-tomcat-6.0.41/webapps/sitestory/WEB-INF/classes/ta.properties
-
-
-For Proxy Server:- Wiki Mirror:
-
 sudo apt-get install apache2-dev
 sudo apt-get install gcc
 sudo apt-get install build-essential
@@ -17,36 +13,3 @@ sudo apt-get install libapr1 libapr1-dev
 sudo apt-get install libcurl4-openssl-dev
 sudo apt-get install openssl libssl-dev
 sudo apt-get install libtool
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-HighPriority - By the end of February.
-
-1. Get the web collelctions to the Solr index.
-2. Very simple type of scoring.
