7d43f2589467195325fd00c0aa39df395a106695
Shivam Maharshi <shivam.maharshi@gmail.com>
2017-03-31 13:41:43 -0400

    Update prepare-mw.sh

--- scripts/cent_os_6.6/prepare-mw.sh
+++ scripts/cent_os_6.6/prepare-mw.sh
@@ -3,8 +3,8 @@ shivam	ALL=(ALL)	ALL
 wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -ivh epel-release-6-8.noarch.rpm
 yum install -y httpd mysql-server php php-mysql
-yum --enablerepo=extras install epel-release
-yum --enablerepo=epel install phpmyadmin
+yum --enablerepo=extras install -y epel-release
+yum --enablerepo=epel install -y phpmyadmin
 setsebool httpd_can_network_connect_db=1 
 mysqladmin -u root password 'root'
 service mysqld restart
