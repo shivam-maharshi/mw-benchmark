7540fa1990ff6091a8e065887f95ba77b33b326e
Shivam Maharshi <shivam.maharshi@gmail.com>
2017-04-24 18:02:21 -0400

    Rename get-monthly-revision-data-size.sql to get-monthly-write-size.sql

--- sql/get-monthly-revision-data-size.sql
+++
@@ -1,3 +0,0 @@
-use wiki;
-
-SELECT rev_page, rev_len, rev_timestamp FROM revision r WHERE r.rev_timestamp like '201510%' ORDER BY r.rev_page ASC, r.rev_timestamp ASC INTO OUTFILE '/tmp/rev_data_size.txt' FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
