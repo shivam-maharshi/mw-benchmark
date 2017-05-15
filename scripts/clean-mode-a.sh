aaba34a1dc5cb421b6158e77361df9f3398beac1
Shivam Maharshi <shivam.maharshi@gmail.com>
2017-05-15 17:28:19 -0400

    Update clean-mode-a.sh

--- scripts/clean-mode-a.sh
+++ scripts/clean-mode-a.sh
@@ -1,3 +1,3 @@
 sudo rm -rf /var/cache/wiki/*
-sudo /usr/sbin/httpd -f /etc/httpd/conf/httpd-a.conf -k start
+sudo /usr/sbin/httpd -f /etc/httpd/conf/httpd-a.conf -k restart
 sudo service mysqld restart
