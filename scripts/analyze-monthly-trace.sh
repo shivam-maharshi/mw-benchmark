61b4efb6d9ca5b93cfd65c8f07e4843726700a62
Shivam Maharshi <shivam.maharshi@gmail.com>
2017-05-15 17:33:19 -0400

    Update analyze-monthly-trace.sh

--- scripts/analyze-monthly-trace.sh
+++ scripts/analyze-monthly-trace.sh
@@ -1,2 +1,2 @@
 cd ~/benchmarking/mw-benchmark/jar
-sudo java -Xms8096m -Xmx12086m -cp "mw-benchmark-0.0.jar" org.vt.edu.analysis.AnalyzeMonthlyTrace
+java -Xms8096m -Xmx12086m -cp "mw-benchmark-0.0.jar" org.vt.edu.analysis.AnalyzeMonthlyTrace
