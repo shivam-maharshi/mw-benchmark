3f4c9a9f137155e60dba1a16015e3ce2fcb29878
Shivam Maharshi <shivam.maharshi@gmail.com>
2017-05-15 17:46:18 -0400

    Update populate-mw.sh

--- scripts/cent_os_6.6/populate-mw.sh
+++ scripts/cent_os_6.6/populate-mw.sh
@@ -1,14 +1,14 @@
-sudo chmod 777 -R /var/www/html/mediawiki
+chmod 777 -R /var/www/html/mediawiki
 cd /var/www/html/mediawiki
-sudo echo "$wgEnableAPI=true;" >> LocalSettings.php
-sudo echo "$wgEnableWriteAPI=true;" >> LocalSettings.php
-sudo php maintenance/rebuildall.php
-sudo /etc/init.d/apache2 restart
+echo "$wgEnableAPI=true;" >> LocalSettings.php
+echo "$wgEnableWriteAPI=true;" >> LocalSettings.php
+php maintenance/rebuildall.php
+/etc/init.d/apache2 restart
 cd /home/shivam
-sudo wget "http://archive.org/download/elwiki-20151201/elwiki-20151201-pages-meta-current.xml.bz2"
-sudo wget "http://archive.org/download/elwiki-20151201/elwiki-20151201-all-titles-in-ns0.gz"
-sudo bzip2 -dk elwiki-20151201-pages-meta-current.xml.bz2
+wget "http://archive.org/download/elwiki-20151201/elwiki-20151201-pages-meta-current.xml.bz2"
+wget "http://archive.org/download/elwiki-20151201/elwiki-20151201-all-titles-in-ns0.gz"
+bzip2 -dk elwiki-20151201-pages-meta-current.xml.bz2
 cd /var/www/html/mediawiki/maintenance
 # sudo php importDump.php < /home/ubuntu/elwiki-20151201-pages-meta-history.xml >& progress.log &
 java -Xmx16396m -Xms1024m -XX:+UseParallelGC -server -jar mwdumper.jar --format=sql:1.5 /home/shivam/elwiki-20151201-pages-meta-current.xml.bz2 | mysql -u root -p wikimirror --default-character-set=utf8
-sudo tail -f progress.log
+tail -f progress.log
