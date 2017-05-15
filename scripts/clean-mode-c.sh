22509e477776bec12c8b32bfc8a357601ce68b98
Shivam Maharshi <shivam.maharshi@gmail.com>
2017-05-15 17:29:47 -0400

    Update clean-mode-c.sh

--- scripts/clean-mode-c.sh
+++ scripts/clean-mode-c.sh
@@ -1,3 +1,3 @@
-sudo rm -rf /var/cache/wiki/*
-sudo /usr/sbin/httpd -f /etc/httpd/conf/httpd-c.conf -k restart
-sudo service mysqld restart
+rm -rf /var/cache/wiki/*
+/usr/sbin/httpd -f /etc/httpd/conf/httpd-c.conf -k restart
+service mysqld restart
