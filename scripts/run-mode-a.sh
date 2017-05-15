0cf99d78c0cda4325a3717d80863038f38fe6af6
Shivam Maharshi <shivam.maharshi@gmail.com>
2017-05-15 17:29:01 -0400

    Update and rename run-modea.sh to run-mode-a.sh

---
+++ scripts/run-mode-a.sh
@@ -0,0 +1,5 @@
+rm /home/shivam/monitor/resources.txt
+touch /home/shivam/monitor/resources.txt
+chmod -R 777 /home/shivam/monitor/resources.txt
+sh /home/shivam/mr.sh
+sh /home/shivam/logstash.sh &
