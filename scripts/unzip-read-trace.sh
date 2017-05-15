22aa81d120878edb553baa4d594c1d5ae830ea74
Shivam Maharshi <shivam.maharshi@gmail.com>
2017-05-15 17:44:25 -0400

    Update unzip-read-trace.sh

--- scripts/unzip-read-trace.sh
+++ scripts/unzip-read-trace.sh
@@ -1,2 +1,2 @@
 cd ~/benchmarking/mw-benchmark/jar
-sudo java -Xms8096m -Xmx12086m -cp "mw-benchmark-0.0.jar" org.vt.edu.trace.UnzipMonthlyTrace
+java -Xms8096m -Xmx12086m -cp "mw-benchmark-0.0.jar" org.vt.edu.trace.UnzipMonthlyTrace
